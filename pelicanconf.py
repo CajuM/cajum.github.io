f2aa8b2ad967caecfa57538374ea3b6ed3bef152
Mihai-Drosi Câju <bug@me.not>
2025-01-02 18:05:45 +0200

    Initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+AUTHOR = 'Câju Mihai-Drosi'
+SITENAME = "Câju Mihai-Drosi's blog"
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Europe/Bucharest'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+)
+
+# Social widget
+SOCIAL = (
+    ("LinkedIn", "https://www.linkedin.com/in/mihai-drosi-c%C3%A2ju-92795233b/"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
